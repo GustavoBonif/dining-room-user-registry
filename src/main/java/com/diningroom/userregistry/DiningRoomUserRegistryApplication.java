179a6d9a8886a6a77d8de0b0186f4edf6db2e21c
GustavoBonif <gubonic@gmail.com>
2023-11-26 18:48:13 -0300

    setup: creating project

---
+++ src/main/java/com/diningroom/userregistry/DiningRoomUserRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.diningroom.userregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningRoomUserRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningRoomUserRegistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diningroom/userregistry/DiningRoomUserRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diningroom.userregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningRoomUserRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
